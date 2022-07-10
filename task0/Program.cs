195b2bf6a5962af715854b8e4a39c2384b3966e6
InEmEcI <32899349+InEmEcI@users.noreply.github.com>
2022-07-11 00:08:52 +0300

    task0 на Linux не работает!

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("Ваедите число: ");
+
+
+string inputLine = Console.Read.Line();
+if (inputLine != null) {
+    Console.Write("Вы ввели  ");
+    Console.Write(inputLine);
+}
+
+/*
+if (inputLine != null) {
+    int inputNumber = int.Parse(inputLine);
+    int outputNumber = (int)Math.Pow(inputNumber,2);
+    Console.Write("В квадрате оно = ");
+    Console.Write(outputNumber);
+}
+*/
