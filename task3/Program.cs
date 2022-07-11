67422bb2110c5dadff48b6d528d8c3b1ca4c9dd7
InEmEcI <iikar@bk.ru>
2022-07-11 22:59:15 +0300

    Добавил task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.Write("Введите число: ");
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) 
+{
+
+    int inputNumber = int.Parse(inputLine);
+
+    int startNumber = inputNumber * -1;
+
+    string lineOutput = "";
+
+    while (startNumber < inputNumber)
+    {
+        lineOutput = lineOutput + startNumber + ", ";
+        startNumber++;
+    }
+    lineOutput = lineOutput + inputNumber;
+    Console.Write(lineOutput);
+};
