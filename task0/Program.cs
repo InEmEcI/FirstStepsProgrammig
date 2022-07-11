b18047ac348fe0a476233b3a2bbd0513cc1cf3ee
InEmEcI <iikar@bk.ru>
2022-07-11 14:48:13 +0300

    Исправил ошибку в коде

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,19 +1,14 @@
 ﻿
-
+Console.Clear();
 Console.Write("Ваедите число: ");
 
 
-string inputLine = Console.Read.Line();
-if (inputLine != null) {
-    Console.Write("Вы ввели  ");
-    Console.Write(inputLine);
-}
+string? inputLine = Console.ReadLine();
 
-/*
 if (inputLine != null) {
     int inputNumber = int.Parse(inputLine);
     int outputNumber = (int)Math.Pow(inputNumber,2);
     Console.Write("В квадрате оно = ");
     Console.Write(outputNumber);
 }
-*/
+
