0f708a09bf6f22223541fa6288760c36a753fdd6
InEmEcI <iikar@bk.ru>
2022-07-12 11:07:43 +0300

    Добавил другой вариант решения task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -6,9 +6,28 @@ Console.Write("Ваедите число: ");
 string? inputLine = Console.ReadLine();
 
 if (inputLine != null) {
+    char[] M = inputLine.ToCharArray();
+    Console.Write(M[2]);
+}
+
+/*
+    if (inputLine != null) {
     int inputNumber = int.Parse(inputLine);
     int outputNumber = inputNumber%10;
     Console.Write("Остаток от деления на 10 = ");
     Console.Write(outputNumber);
 }
+*/
+
+
+/*
+if (inputLine != null) {
+    int inputNumber = int.Parse(inputLine);
+    int outputNumber = inputNumber%10;
+    Console.Write("Остаток от деления на 10 = ");
+    Console.Write(outputNumber);
+}
+*/
+
+
 
