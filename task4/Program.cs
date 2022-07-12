51a0d0c40e3cb6a47bfb7cc57685ee3639ecd021
InEmEcI <iikar@bk.ru>
2022-07-12 10:36:09 +0300

    Добавил task4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.Write("Ваедите число: ");
+
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) {
+    int inputNumber = int.Parse(inputLine);
+    int outputNumber = inputNumber%10;
+    Console.Write("Остаток от деления на 10 = ");
+    Console.Write(outputNumber);
+}
+
