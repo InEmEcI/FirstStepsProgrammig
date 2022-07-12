63ef99ee1fc41811f672cfcda14414fd5bb74759
InEmEcI <iikar@bk.ru>
2022-07-12 17:27:35 +0300

    Домашнее задание - Задача 6

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) 
+{
+    int inputNumber = int.Parse(inputLine);
+    int ostatok = inputNumber%2;
+    if (ostatok == 0){
+        Console.Write("Чётное!");
+    } else {
+        Console.Write("Не чётное!");
+    }
+}
