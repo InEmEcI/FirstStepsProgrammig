6d93c4ccc5d206f8df92a893025135ef4b1d804f
InEmEcI <iikar@bk.ru>
2022-07-12 17:54:23 +0300

    Домашнее задание - Задача 8

---
+++ task8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.Clear();
+Console.Write("Введите число (от 1 ...): ");
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) 
+{
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber < 1) {
+       Console.Write("Ошибка ввода!"); 
+    } else {
+        int B = 2;
+        string line = "";
+        while (B < inputNumber){
+            line = line + B + ", ";
+            B = B + 2;
+        }
+        Console.WriteLine("Все чётные от 1 до " + inputNumber + ":");
+        Console.WriteLine(line);
+    }
+
+}
