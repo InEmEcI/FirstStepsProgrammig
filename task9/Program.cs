efac0d5f26768b7d6b19234e78b766b94c9c70e7
InEmEcI <iikar@bk.ru>
2022-07-13 12:27:41 +0300

    Добавил task9

---
+++ task9/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.Clear();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Вариант 1");
+System.Random numberSintezator = new Random();
+int number = numberSintezator.Next(10,100);
+
+Console.WriteLine(number);
+
+int firstNumber = number / 10;
+int secondNumber = number%10;
+
+// Вариант 1
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);    
+} else 
+{
+    Console.WriteLine(secondNumber);
+}
+ 
+// Вариант 2
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Вариант 2");
+int numberKirilla = new Random().Next(10, 100);
+Console.Write("The random number is: ");
+Console.WriteLine(numberKirilla);
+string stringNum = numberKirilla.ToString();
+Console.Write("The biggest digit is: ");
+if (stringNum[0] > stringNum[1]){
+    Console.WriteLine(stringNum[0]);
+} else {
+    Console.WriteLine(stringNum[1]);
+}
+
+// Вариант 3
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Вариант 3");
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+
+Console.WriteLine("firstNumber is: "); firstNumber = ((int)digits[0]-48);
+Console.WriteLine("secondNumber is: "); secondNumber = ((int)digits[1]-48); 
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+Console.Write("Result is: "); Console.Write(resultNumber);
