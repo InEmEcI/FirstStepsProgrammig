00a2abab2d7dc80e8a5f2dbe93071a91678d5639
InEmEcI <iikar@bk.ru>
2022-07-13 13:27:00 +0300

    Обернул каждый вариант task9 в метод

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,8 +1,14 @@
 ﻿
 Console.Clear();
-Console.WriteLine("-----------------------------");
-Console.WriteLine("Вариант 1");
+
 System.Random numberSintezator = new Random();
+
+// Вариант 1
+void variant1()
+{
+Console.WriteLine("-----------------------------");    
+Console.WriteLine("Вариант 1");
+
 int number = numberSintezator.Next(10,100);
 
 Console.WriteLine(number);
@@ -10,7 +16,6 @@ Console.WriteLine(number);
 int firstNumber = number / 10;
 int secondNumber = number%10;
 
-// Вариант 1
 if (firstNumber > secondNumber)
 {
     Console.WriteLine(firstNumber);    
@@ -18,8 +23,12 @@ if (firstNumber > secondNumber)
 {
     Console.WriteLine(secondNumber);
 }
- 
+} 
+variant1();
+
 // Вариант 2
+void variant2()
+{
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Вариант 2");
 int numberKirilla = new Random().Next(10, 100);
@@ -32,17 +41,23 @@ if (stringNum[0] > stringNum[1]){
 } else {
     Console.WriteLine(stringNum[1]);
 }
+};
+variant2();
 
 // Вариант 3
+void variant3()
+{
 Console.WriteLine("-----------------------------");
 Console.WriteLine("Вариант 3");
 char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
 
 Console.Write("Рандомное число: "); Console.WriteLine(digits); 
 
-firstNumber = ((int)digits[0]-48);
-secondNumber = ((int)digits[1]-48); 
+int firstNumber = ((int)digits[0]-48);
+int secondNumber = ((int)digits[1]-48); 
 
 int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
 Console.Write("бОльшее из его цифр: "); Console.WriteLine(resultNumber);
 Console.WriteLine("-----------------------------");
+} 
+variant3();
