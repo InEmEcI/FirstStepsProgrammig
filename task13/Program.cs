4874e83a2ea6cada80d44bf26efdc4492c4147ad
InEmEcI <iikar@bk.ru>
2022-07-14 17:22:08 +0300

    Домашнее задание - Задача 10

---
+++ task13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+string? inputLine = Console.ReadLine();
+
+void secondDigitOfTheNumber()
+{
+if (inputLine != null) 
+{
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber < 100 || inputNumber > 999) // является ли введённое число трёхзначным ?
+        {
+            Console.Write("Ошибка ввода!");
+        } else 
+        {
+            string stringNumber = inputNumber.ToString();
+            char[] arr = stringNumber.ToCharArray();
+            Console.Write("Вторая цифра = ");    
+            Console.Write(arr[1]);
+        }
+}   
+}
+secondDigitOfTheNumber();
+
+
