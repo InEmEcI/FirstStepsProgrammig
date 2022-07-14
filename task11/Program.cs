a8f41857addcd58ea8cc2b28d554d930afb61b03
InEmEcI <iikar@bk.ru>
2022-07-14 10:59:23 +0300

    добавил Решение от Ирины

--- task11/Program.cs
+++ task11/Program.cs
@@ -2,6 +2,29 @@
 
 // Добавить Решение от Ирины (на видео 1ч. 33м)
 
+Console.WriteLine("Является ли первое число квадратом второго?");
+Console.Write("Введите первое число: ");
+string? inputLinkOne = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? inputLinkTwo = Console.ReadLine();
+
+
+if (inputLinkOne != null && inputLinkTwo != null)
+{
+    int first = int.Parse(inputLinkOne);
+    int second = int.Parse(inputLinkTwo);
+    int c = second % first;
+    string iext = "";
+    Console.WriteLine(iext);
+    if (c == 0)
+    {
+        iext = "Да. Второе число кратно первому.";
+        Console.WriteLine(iext);
+    } else {
+        iext = "Нет. Второе число не кратно первому. Остаток от деления: " + c;
+        Console.WriteLine(iext);
+    }    
+}
 // Решение от Артёма
 /*
 Console.WriteLine("Является ли первое число квадратом второго?");
