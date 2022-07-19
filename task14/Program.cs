da7917f07435ca124c8c00c68f1169c9bb325934
InEmEcI <iikar@bk.ru>
2022-07-19 19:12:04 +0300

    Добавл проверку введённого числа начинающегося на ноль

--- task14/Program.cs
+++ task14/Program.cs
@@ -5,65 +5,115 @@
 
 
 Console.Clear();
-
+/*
 void thirdDigitVariant1()
-{    
-Console.WriteLine("--------------------");
-Console.WriteLine("Вариант 1");
-Console.Write("Введите число: ");
-string? inputLine = Console.ReadLine();
+{
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Вариант 1");
+    Console.Write("Введите число: ");
+    string? inputLine = Console.ReadLine();
 
-{
-if (inputLine != null) 
-{
-    int inputNumber = int.Parse(inputLine);
-    if (inputNumber < 100) // проверяет есть ли у числа третья цифра
+    {
+        if (inputLine != null)
         {
-            Console.WriteLine("Третьей цифры нет");
-            Console.WriteLine("--------------------");
-        } else 
-        {
-            string stringNumber = inputNumber.ToString();
-            char[] arr = stringNumber.ToCharArray();
-            Console.Write("Третья цифра = ");    
-            Console.WriteLine(arr[2]);
-            Console.WriteLine("--------------------");                     
+            int inputNumber = int.Parse(inputLine);
+            if (inputNumber < 100) // проверяет есть ли у числа третья цифра
+            {
+                Console.WriteLine("Третьей цифры нет");
+                Console.WriteLine("--------------------");
+            }
+            else
+            {
+                string stringNumber = inputNumber.ToString();
+                char[] arr = stringNumber.ToCharArray();
+                Console.Write("Третья цифра = ");
+                Console.WriteLine(arr[2]);
+                Console.WriteLine("--------------------");
+            }
         }
+    }
 }
-}
-}
+*/
 
-
-
-
-
-
-
+/*
 void thirdDigitVariant2()
 {
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Вариант 2");
+    Console.Write("Введите число: ");
+    string? inputLine2 = Console.ReadLine();
+
+    else
+    {
+
+        if (inputLine2 != null)
+        {
+
+            int inputNumber = int.Parse(inputLine2);
+            if (inputNumber < 100) // проверяет есть ли у числа третья цифра
+            {
+                Console.WriteLine("Третьей цифры нет");
+                Console.WriteLine("--------------------");
+            }
+            else
+            {
+                double value = (inputNumber / 10) % 10;
+                Console.WriteLine(value);
+            }
+            //int thirdТumber = inputNumber % 10;
+            //может цикл ? 
+            
+            int thirdТumber = inputNumber % 10; // вычисляет третью цифру введённого числа        
+            Console.Write("Третья цифры: ");
+            Console.WriteLine(thirdТumber);
+            Console.WriteLine("--------------------");
+            
+
+
 Console.WriteLine("Вариант 2");
 Console.Write("Введите число: ");
-string? inputLine1 = Console.ReadLine();
+string? inputLine2 = Console.ReadLine();
 
+
+
+    }
+}
+*/
+
+
+
+void variant3()
 {
-if (inputLine1 != null) 
-{
-    int inputNumber = int.Parse(inputLine1);
-    if (inputNumber < 100) // проверяет есть ли у числа третья цифра
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Вариант 3");
+    Console.Write("Введите число: ");
+    string? inputLine3 = Console.ReadLine();
+
+
+    if (inputLine3 != null)
+    {
+        // проверка на ввод числа начинающегося на 0
+        //int inputNumber2 = int.Parse(inputLine2);
+        char[] arr3 = inputLine3.ToString().ToCharArray();
+        //string stringNumber2 = inputNumber2.ToString();
+        Console.WriteLine(arr3);
+        
+        if (arr3[0] == '0')
         {
-            Console.WriteLine("Третьей цифры нет");
-            Console.WriteLine("--------------------");
-        } else 
-        {        
-         int thirdТumber = inputNumber % 10; // вычисляет третью цифру введённого числа        
-         Console.Write("Третья цифры: ");
-         Console.WriteLine(thirdТumber);
-         Console.WriteLine("--------------------");
+            //Console.Write("Число не должно начинаться на 0!");
+            Console.WriteLine("Число начинается на 0!");
         }
-}
-}
+        else
+        {
+            Console.WriteLine("Число начинается НЕ на 0!");
+        }
+    }
 }
 
 
-thirdDigitVariant1();
-thirdDigitVariant2();
+
+
+
+//thirdDigitVariant1();
+//thirdDigitVariant2();
+variant3();
