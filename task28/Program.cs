edcbd55f8e003e358b8a3e086c5838b208c1faf0
InEmEcI <iikar@bk.ru>
2022-08-04 13:56:35 +0300

    Добавил task28 - без Рекурсии

---
+++ task28/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Задача №28
+// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// Например:
+// 4 -> 24
+// 5 -> 120
+// 
+// С * - решить эту задачу с помощью Рекурсии !
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string? inpitLine = Console.ReadLine();
+int inputNum = int.Parse(inpitLine?? "");
+
+Console.WriteLine(sumNums(inputNum));
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        sum *= i;
+        // The same
+        // sum = sum * i;
+    }
+    return sum;
+}
+
+
+
+
+
+
