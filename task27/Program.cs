39aee0998a3a5250703929a5a6dbe6e1fd616da5
InEmEcI <iikar@bk.ru>
2022-08-04 21:28:59 +0300

    Домашнее задание Задача 27 (пока без * )

---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// №27 Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// * Сделать оценку времени алгоритма через вещественные числа и строки
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+long a = long.Parse(inputLine ?? "");
+long b = 0;
+
+long calculation()
+{
+    while (a > 0)
+    {
+        b = b + a % 10;
+        a = a / 10;
+    }
+    // Console.Write(b);
+    return(b);
+}
+
+Console.Write(calculation());
