f1f626baae86adde18db2ceb3f48fbdae9922f5d
InEmEcI <iikar@bk.ru>
2022-08-24 10:01:08 +0300

    Добавил 63, 65, 67, 69

---
+++ task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 67
+// Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.Clear();int inputNumber = ReadNumber("Введите число : ");
+
+Console.Write(SumNumbers(inputNumber, 0));
+
+int SumNumbers(int num, int sum)
+{
+    if (num < 10) return num;
+    else
+    {
+        return num % 10 + SumNumbers(num / 10, sum);
+    }
+    
+}
+
+
+int ReadNumber(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine() ?? "");
+}
